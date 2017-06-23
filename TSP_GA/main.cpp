4f3512b54691d6946458c107c71cfe84c81c92c5
Unknown <pi0tr.abic@gmail.com>
2017-06-23 04:00:33 +0200

    Crossover mechanics
    using PMX method desctibed in:
    http://user.ceng.metu.edu.tr/~ucoluk/research/publications/tspnew.pdf

--- TSP_GA/main.cpp
+++ TSP_GA/main.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
-#include<math.h>
+#include<algorithm> //sort
+#include<math.h>    //exponential, sqrt,
+#include<stdlib.h>  //rand
+#include<time.h>
 //#include<random>
 
 using namespace std;
@@ -9,6 +11,7 @@ using namespace std;
 static int PI=3.14159265359;
 extern vector<vector<float> > cities_table;
 extern float fairnessFactor;
+extern float mixingRatio;
 
 class Solution{
 private:
@@ -36,6 +39,8 @@ public:
         breedingChance=2*3*expf((-9*position^2)/(genSize/2)^2)/(genSize/2*sqrt(2*PI));
     }
     float getChance(){return breedingChance;}           //for getting chance value
+
+    vector<int> getDna(){return dna;}
 };
 
 bool fitnessComparator(Solution a, Solution b){return (a.giveFitness()<b.giveFitness());}   //comparator method which checks the fitness values of the compared solutions
@@ -44,8 +49,10 @@ class Breeder{
 protected:
     vector<vector<Solution> > generations;
     int genSize;
+    int currGen;
 public:
     Breeder(int g_size){
+        currGen=0;
         genSize=g_size;
         vector<int> ctList;
         for(int i=0;i<cities_table.size();i++){ctList.push_back(i);}
@@ -59,16 +66,47 @@ public:
         }
     }
 
-    void GenSort(int targetGen){
-    sort(generations[targetGen].begin(),generations[targetGen].end(),fitnessComparator);
+    void GenSort(){
+    sort(generations[currGen].begin(),generations[currGen].end(),fitnessComparator);
     }
 
-    void GenChances(int targetGen){
+    void GenChances(){
         for(int i=0; i<genSize;i++){
-
+            generations[currGen][i].setChance(i,genSize);
         }
     }
 
+    vector<vector<int> > breedingMethod(int fthr_Id, int mthr_Id){      //crossover method
+       vector<int> papa=generations[currGen][fthr_Id].getDna();
+       vector<int> mama=generations[currGen][mthr_Id].getDna();
+       vector<int> childOne;
+       vector<int> childTwo;
+       vector<vector<int> > children;
+       srand(time(NULL));
+       int ctNbr=cities_table.size();
+       int xpoint=rand()%ctNbr;
+       //
+    }
+
+    void breedSolutions(){
+        vector<int> rndTable;
+        vector<Solution> newGen;
+        for(int i=0;i<cities_table.size();i++){
+            int cntr=(int) generations[currGen][i].getChance()*100;
+            for(int j=0; j<=cntr;j++){
+                rndTable.push_back(i);
+            }
+        }
+        int fatherId,motherId;
+        srand(time(NULL));
+        fatherId=rand()%genSize;
+        do{
+        motherId=rand()%genSize;
+        }while(fatherId!=motherId);
+        //
+
+        currGen++;
+    }
 };
 
 int main(){
